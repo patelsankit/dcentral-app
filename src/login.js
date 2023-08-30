6642b18b840b727331bd12ef1e122c4dd32274e7
sankit patel <patelsankit16@gmail.com>
2023-08-30 23:05:49 +0530

    initial commit

--- src/login.js
+++ src/login.js
@@ -12,8 +12,8 @@ const Login = () => {
     e.preventDefault();
 
     // Hardcoded username and password for demonstration
-    const validUsername = "sas";
-    const validPassword = "sass";
+    const validUsername = "patelsankit16@gmail.com";
+    const validPassword = "9574540106";
 
     if (username === validUsername && password === validPassword) {
       // User is authenticated
@@ -27,21 +27,54 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Login</button>
-    </form>
+     <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 shadow-md rounded-md w-96">
+        <h2 className="text-2xl font-semibold mb-4">Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-gray-600">Username</label>
+            <input
+              type="text"
+              className="w-full border rounded px-3 py-2 focus:outline-none focus:border-blue-400"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-600">Password</label>
+            <input
+              type="password"
+              className="w-full border rounded px-3 py-2 focus:outline-none focus:border-blue-400"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+    // <form onSubmit={handleSubmit}>
+    //   <input className="hidden text-center"
+    //     type="text"
+    //     placeholder="Username"
+    //     value={username}
+    //     onChange={(e) => setUsername(e.target.value)}
+    //   />
+    //   <input
+    //     type="password"
+    //     placeholder="Password"
+    //     value={password}
+    //     onChange={(e) => setPassword(e.target.value)}
+    //   />
+    //   <button type="submit">Login</button>
+    // </form>
   );
 };
 
