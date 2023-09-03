cfb21446c4043dfb79d1ac3f9d6ae4951256b1f9
sankit patel <patelsankit16@gmail.com>
2023-09-03 19:39:09 +0530

    login page changes

--- src/login.js
+++ src/login.js
@@ -1,79 +1,7 @@
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import imageone from "./assets/images/bg.svg";
-
-// const Login = () => {
-//   const [username, setUsername] = useState("");
-//   const [password, setPassword] = useState("");
-//   const navigate = useNavigate();
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-
-//     const validUsername = "patelsankit16@gmail.com";
-//     const validPassword = "9574540106";
-
-//     if (username === validUsername && password === validPassword) {
-//       console.log("User authenticated");
-//       // localStorage.setItem('isAuthorized', true);
-//       navigate("/protected");
-//     } else {
-//       // localStorage.clear();
-//       alert("Invalid username or password");
-//     }
-//   };
-
-//   return (
-//     <div className="min-h-screen flex p-5 sm:p-10">
-//      <div className="hidden lg:block">
-//             <img className="w-full h-full" src={imageone} alt={"logo"}/> 
-//     </div>
-//      <div className="flex items-center justify-center w-full">
-//       <div className="bg-white p-8 shadow-md rounded-md w-96 ">
-//         <h2 className="text-2xl font-semibold mb-4">Login</h2>
-//         <form onSubmit={handleSubmit}>
-//           <div className="mb-4">
-//             <label className="block">Username</label>
-//             <input
-//               type="text"
-//               className="w-full border rounded px-3 py-2 focus:outline-none focus:border-blue-400"
-//               placeholder="Username"
-//               value={username}
-//               onChange={(e) => setUsername(e.target.value)}
-//             />
-//           </div>
-//           <div className="mb-4">
-//             <label className="block text-gray-600">Password</label>
-//             <input
-//               type="password"
-//               className="w-full border rounded px-3 py-2 focus:outline-none focus:border-blue-400"
-//               placeholder="Password"
-//               value={password}
-//               onChange={(e) => setPassword(e.target.value)}
-//             />
-//           </div>
-//           <button
-//             type="submit"
-//             className="btnlogin border-0 hover:border-0 w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
-//           >
-//             Login
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-   
-//     </div>
-//   );
-// };
-
-// export default Login;
-
-
-
-// latest tab code
-
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import imageone from "./assets/images/bg.svg";
 
 const Login = () => {
@@ -83,6 +11,7 @@ const Login = () => {
   const [signupUsername, setSignupUsername] = useState("");
   const [signupPassword, setSignupPassword] = useState("");
   const navigate = useNavigate();
+  const [toastVisible, setToastVisible] = useState(false);
 
   const handleTabChange = (tab) => {
     setActiveTab(tab);
@@ -91,21 +20,23 @@ const Login = () => {
   const handleLoginSubmit = (e) => {
     e.preventDefault();
 
-    // Hardcoded username and password for demonstration
     const validUsername = "patelsankit16@gmail.com";
     const validPassword = "9574540106";
 
     if (loginUsername === validUsername && loginPassword === validPassword) {
-      // User is authenticated
-      // Redirect to the protected page or load its content dynamically
       console.log("User authenticated");
-      // Redirect the user to the protected page
       navigate("/protected");
     } else {
-      // Clear form inputs
       setLoginUsername("");
       setLoginPassword("");
-      alert("Invalid username or password");
+
+      if (!toastVisible) {
+        setToastVisible(true);
+        toast.error("Invalid username or password", {
+          autoClose: 3000,
+          onClose: () => setToastVisible(false),
+        });
+      }
     }
   };
 
@@ -120,7 +51,10 @@ const Login = () => {
     setSignupPassword("");
 
     // For demonstration purposes, display a success message
-    alert("Signup successful!");
+    toast.success("signup successfully", {
+      autoClose: 3000,
+      onClose: () => setToastVisible(false),
+    });
   };
 
   return (
@@ -129,98 +63,109 @@ const Login = () => {
         <img className="w-full h-full" src={imageone} alt={"logo"} />
       </div>
       <div className="flex items-center justify-center w-full">
-        <div className="bg-white p-8 shadow-md rounded-md w-96">
+        <div className="bg-white p-8 shadow-md rounded-md w-96 md:w-[450px]">
           <div className="flex justify-between mb-4">
             <button
               className={`${
-                activeTab === "login" ? "bg-blue-500" : "bg-gray-200"
-              } px-4 py-2 rounded-tl-md rounded-bl-md focus:outline-none`}
+                activeTab === "login"
+                  ? "text-blue-500 border-b-4 border-[#7654ff] rounded-full"
+                  : "text-gray-200 border-b border-[#E5E6F0] rounded-full"
+              } px-4 py-2 focus:outline-none uppercase w-1/2`}
               onClick={() => handleTabChange("login")}
             >
               Login
             </button>
             <button
               className={`${
-                activeTab === "signup" ? "bg-blue-500" : "bg-gray-200"
-              } px-4 py-2 rounded-tr-md rounded-br-md focus:outline-none`}
+                activeTab === "signup"
+                  ? "text-blue-500 border-b-4 border-[#7654ff] rounded-full"
+                  : "text-gray-200 border-b border-[#E5E6F0] rounded-full"
+              } px-4 py-2 focus:outline-none uppercase w-1/2`}
               onClick={() => handleTabChange("signup")}
             >
               Signup
             </button>
           </div>
-          {activeTab === "login" && (
-            <form onSubmit={handleLoginSubmit}>
-              <h2 className="text-2xl font-semibold mb-4">Login</h2>
-             <div class="coolinput">
-              <label for="input" class="text">Game Name</label>
-              <input type="text" placeholder="Super Awesome Game" name="input" class="input" />
-            </div>
-             
-             
-             
-              <div className="mb-4">
-                <label className="block">Username</label>
-                <input
-                  type="text"
-                  className="w-full border rounded px-3 py-2 focus:outline-none focus:border-blue-400"
-                  placeholder="Username"
-                  value={loginUsername}
-                  onChange={(e) => setLoginUsername(e.target.value)}
-                />
-              </div>
-
-
-              <div className="mb-4">
-                <label className="block text-gray-600">Password</label>
-                <input
-                  type="password"
-                  className="w-full border rounded px-3 py-2 focus:outline-none focus:border-blue-400"
-                  placeholder="Password"
-                  value={loginPassword}
-                  onChange={(e) => setLoginPassword(e.target.value)}
-                />
-              </div>
-              <button
-                type="submit"
-                className="btnlogin border-0 hover:border-0 w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
-              >
-                Login
-              </button>
-            </form>
-          )}
-          {activeTab === "signup" && (
-            <form onSubmit={handleSignupSubmit}>
-              <h2 className="text-2xl font-semibold mb-4">Signup</h2>
-              <div className="mb-4">
-                <label className="block">Username</label>
-                <input
-                  type="text"
-                  className="w-full border rounded px-3 py-2 focus:outline-none focus:border-blue-400"
-                  placeholder="Username"
-                  value={signupUsername}
-                  onChange={(e) => setSignupUsername(e.target.value)}
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-600">Password</label>
-                <input
-                  type="password"
-                  className="w-full border rounded px-3 py-2 focus:outline-none focus:border-blue-400"
-                  placeholder="Password"
-                  value={signupPassword}
-                  onChange={(e) => setSignupPassword(e.target.value)}
-                />
-              </div>
-              <button
-                type="submit"
-                className="btnlogin border-0 hover:border-0 w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
-              >
-                Signup
-              </button>
-            </form>
-          )}
+          <div className="content">
+            {activeTab === "login" && (
+              <form onSubmit={handleLoginSubmit}>
+                <h2 className="text-2xl font-semibold mb-4">Login</h2>
+                <div className="coolinput mb-4">
+                  <label htmlFor="input" className="text">
+                    Game Name
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Super Awesome Game"
+                    name="input"
+                    className="input"
+                    value={loginUsername}
+                    onChange={(e) => setLoginUsername(e.target.value)}
+                  />
+                </div>
+                <div className="coolinput mb-4">
+                  <label htmlFor="input" className="text">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    name="input"
+                    className="input"
+                    value={loginPassword}
+                    onChange={(e) => setLoginPassword(e.target.value)}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="btnlogin border-0 rounded-[33px] hover:border-0 w-full bg-[#121051] text-white py-2"
+                  disabled={toastVisible}
+                >
+                  Login
+                </button>
+              </form>
+            )}
+            {activeTab === "signup" && (
+              <form onSubmit={handleSignupSubmit}>
+                <h2 className="text-2xl font-semibold mb-4">Signup</h2>
+                <div className="coolinput mb-4">
+                  <label htmlFor="input" className="text">
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Username"
+                    name="input"
+                    className="input"
+                    value={signupUsername}
+                    onChange={(e) => setSignupUsername(e.target.value)}
+                  />
+                </div>
+                <div className="coolinput mb-4">
+                  <label htmlFor="input" className="text">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    name="input"
+                    className="input"
+                    value={signupPassword}
+                    onChange={(e) => setSignupPassword(e.target.value)}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="btnlogin border-0 rounded-[33px] hover:border-0 w-full bg-[#121051] text-white py-2"
+                >
+                  Signup
+                </button>
+              </form>
+            )}
+          </div>
         </div>
       </div>
+      <ToastContainer position="bottom-right" />
     </div>
   );
 };
